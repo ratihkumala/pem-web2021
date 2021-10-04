3fe805ed953c6aad89a0f81d083c30e5d90806a9
ratihkumala <111202012520@mhs.dinus.ac.id>
2021-10-05 04:24:23 +0700

    Document

---
+++ repo-minggu4/proses.php
@@ -0,0 +1,10 @@
+ <?php
+  echo $_POST['nama'];
+  echo "<br />";
+  echo $_POST['email'];
+  echo "<br />";
+  echo $_POST['notlp'];
+  echo "<br />";
+  echo $_POST['komentar'];
+  echo "<br />";
+?>
